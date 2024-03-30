71f309d3fcb1e76c3fd48c274d7a7fe23eaff159
Jefferson Celestino <jefferson.ucb02@gmail.com>
2024-03-30 17:23:19 -0300

    Component elementfield

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import ButtonPrimary from "./components/ButtonPrimary";
+import ElementField from "./components/ElementField";
 import Header from "./components/Header";
 
 export default function App() {
@@ -6,6 +7,7 @@ export default function App() {
   return (
     <>
       <Header />
+      <ElementField />
       <ButtonPrimary text="Ver promoção"/>
     </>
   )
---
+++ src/components/ElementField/index.tsx
@@ -0,0 +1,9 @@
+import './style.css';
+
+export default function ElementField() {
+    return (
+        <div className='rm-container-elementfield '>
+            <h1>Página inicial</h1>
+        </div>
+    );
+}
